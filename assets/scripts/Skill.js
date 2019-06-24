7b8e7eddddd05d3a7f708ae690a0dd547f673123
DESKTOP-8O8FFFA\81028 <fiaon>
2019-06-24 17:37:25 +0800

    添加技能CD

--- assets/scripts/Skill.js
+++ assets/scripts/Skill.js
@@ -41,97 +41,100 @@ cc.Class({
         this.dir = cc.v2(0,0);
         var line = this.target.getChildByName("line");
         this.Rocker.on(cc.Node.EventType.TOUCH_START,function(e){
-      
-            line.active = true;
-            var w_pos = e.getLocation();
-            var pos = this.node.convertToNodeSpaceAR(w_pos);
-            var len = pos.mag();//获取向量长度
-            this.dir.x = pos.x / len;
-            this.dir.y = pos.y / len;
-
-            if(len > this.Max_r){
-                pos.x = this.Max_r * pos.x / len;
-                pos.y = this.Max_r * pos.y / len;
+            if(!this.is_Cd){
+                line.active = true;
+                var w_pos = e.getLocation();
+                var pos = this.node.convertToNodeSpaceAR(w_pos);
+                var len = pos.mag();//获取向量长度
+                this.dir.x = pos.x / len;
+                this.dir.y = pos.y / len;
+    
+                if(len > this.Max_r){
+                    pos.x = this.Max_r * pos.x / len;
+                    pos.y = this.Max_r * pos.y / len;
+                }
+                this.Rocker.setPosition(pos);
             }
-            this.Rocker.setPosition(pos);
             
         },this);
    
         this.Rocker.on(cc.Node.EventType.TOUCH_MOVE,function(e){
-            
-            var w_pos = e.getLocation();
-            var pos = this.node.convertToNodeSpaceAR(w_pos);
-
-            var len = pos.mag();
-            this.dir.x = pos.x / len;
-            this.dir.y = pos.y / len;
-
-            if(len > this.Max_r){
-                pos.x = this.Max_r * pos.x / len;
-                pos.y = this.Max_r * pos.y / len;
+            if(!this.is_Cd){
+                var w_pos = e.getLocation();
+                var pos = this.node.convertToNodeSpaceAR(w_pos);
+                var len = pos.mag();
+                this.dir.x = pos.x / len;
+                this.dir.y = pos.y / len;
+    
+                if(len > this.Max_r){
+                    pos.x = this.Max_r * pos.x / len;
+                    pos.y = this.Max_r * pos.y / len;
+                }
+                this.Rocker.setPosition(pos);
             }
-            this.Rocker.setPosition(pos);
             
         },this);
    
         this.Rocker.on(cc.Node.EventType.TOUCH_END,function(e){
-            
-            var r = Math.atan2(this.dir.y,this.dir.x);
-            var degree = r * 180/(Math.PI);
-            degree = 360 - degree + 90;
-            //(0~180)抢朝向是正的(180~360)抢换个方向
-            var rotationnum = Math.abs(degree)%360;
-            if(rotationnum>0&&rotationnum<180){
-                //TODO
-            }else{
-                
+            if(!this.is_Cd){
+                var r = Math.atan2(this.dir.y,this.dir.x);
+                var degree = r * 180/(Math.PI);
+                degree = 360 - degree + 90;
+                //(0~180)抢朝向是正的(180~360)抢换个方向
+                var rotationnum = Math.abs(degree)%360;
+                if(rotationnum>0&&rotationnum<180){
+                    //TODO
+                }else{
+                    
+                }
+                // TODO 开炮
+                line.active = false;
+                var skillbullet = cc.instantiate(this.skill_bullet);
+                let data = {
+                    width:line.width,
+                }
+                skillbullet.getComponent("Bullet").init(data);
+                this.target.addChild(skillbullet);
+                //cd
+                this.is_Cd = true;
+    
+                this.Rocker.setPosition(cc.v2(0,0));
+                this.dir = cc.v2(0, 0);
+                this.scheduleOnce(function() {
+                    this.target.rotation = 0;
+                }, 0.9);
             }
-            // TODO 开炮
-            line.active = false;
-            var skillbullet = cc.instantiate(this.skill_bullet);
-            let data = {
-                width:line.width,
-            }
-            skillbullet.getComponent("Bullet").init(data);
-            this.target.addChild(skillbullet);
-            //cd
-            this.is_Cd = true;
-
-            this.Rocker.setPosition(cc.v2(0,0));
-            this.dir = cc.v2(0, 0);
-            this.scheduleOnce(function() {
-                this.target.rotation = 0;
-            }, 0.5);
         },this);
    
         this.Rocker.on(cc.Node.EventType.TOUCH_CANCEL,function(e){
-
-            var r = Math.atan2(this.dir.y,this.dir.x);
-            var degree = r * 180/(Math.PI);
-            degree = 360 - degree + 90;
-            //(0~180)抢朝向是正的(180~360)抢换个方向
-            var rotationnum = Math.abs(degree)%360;
-            if(rotationnum>0&&rotationnum<180){
-                //TODO
-            }else{
+            if(!this.is_Cd){
+                var r = Math.atan2(this.dir.y,this.dir.x);
+                var degree = r * 180/(Math.PI);
+                degree = 360 - degree + 90;
+                //(0~180)抢朝向是正的(180~360)抢换个方向
+                var rotationnum = Math.abs(degree)%360;
+                if(rotationnum>0&&rotationnum<180){
+                    //TODO
+                }else{
+                    
+                }
+                // TODO 开炮
+                line.active = false;
+                var skillbullet = cc.instantiate(this.skill_bullet);
+                let data = {
+                    width:line.width,
+                }
+                skillbullet.getComponent("Bullet").init(data);
+                this.target.addChild(skillbullet);
+                //cd
+                this.is_Cd = true;
                 
+                this.Rocker.setPosition(cc.v2(0,0));
+                this.dir = cc.v2(0, 0);
+                this.scheduleOnce(function() {
+                    this.target.rotation = 0;
+                }, 0.9);
             }
-            // TODO 开炮
-            line.active = false;
-            var skillbullet = cc.instantiate(this.skill_bullet);
-            let data = {
-                width:line.width,
-            }
-            skillbullet.getComponent("Bullet").init(data);
-            this.target.addChild(skillbullet);
-            //cd
-            this.is_Cd = true;
-            
-            this.Rocker.setPosition(cc.v2(0,0));
-            this.dir = cc.v2(0, 0);
-            this.scheduleOnce(function() {
-                this.target.rotation = 0;
-            }, 0.5);
             
         },this);
     },
